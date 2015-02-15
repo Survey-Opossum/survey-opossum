a676cbb32a37db8248be92574a5b21829bed8b43
Bernard <worthybernard@gmail.com>
2015-02-14 20:05:19 -0500

    Don't know

--- app/assets/javascripts/surveys.js
+++ app/assets/javascripts/surveys.js
@@ -1,4 +1,4 @@
-jQuery(function () {
+$(function () {
 
   if ($('.short-answer-template').length > 1) {
     console.log('more than one');
@@ -10,7 +10,7 @@ jQuery(function () {
   $('.add-btn').click(function() {
 
     var clone = $('.short-answer-template:last').clone();
-    // console.log(inputArr);
+
     clone.insertBefore('.actions');
 
     var questionFieldInputs = $(".short-answer-template:last > .question_fields");
@@ -18,17 +18,21 @@ jQuery(function () {
     var fieldsArr = $('.field', questionFieldInputs);
     var labelArr = $('label', questionFieldInputs);
     var textareaArr = $('textarea', questionFieldInputs);
-    console.log(textareaArr);
+    var fieldInputArr = $('input', fieldsArr[2]);
+
+    // console the
+    console.log(fieldInputArr[0].value);
+
+    // fieldInputArr[0].value = Number(fieldInputArr[0].value) + 1;
 
     for (i = 0; i < textareaArr.length; i++) {
       if (textareaArr[i].id) {
         var str = textareaArr[i].id;
         var patt1 = /\d+/g;
         var result = str.match(patt1);
+
         result = Number(result) + 1;
         textareaArr[i].id = str.replace(str.match(patt1), result);
-        console.log(textareaArr[i].id);
-
         textareaArr[i].value = '';
       }
 
@@ -36,55 +40,81 @@ jQuery(function () {
         var str = textareaArr[i].name;
         var patt1 = /\d+/g;
         var result = str.match(patt1);
+
         result = Number(result) + 1;
         textareaArr[i].name = str.replace(str.match(patt1), result);
-        console.log(textareaArr[i].name);
       }
     }
-
-    console.log(labelArr);
 
     for (i = 0; i < labelArr.length; i++) {
       if (labelArr[i].htmlFor) {
         var str = labelArr[i].htmlFor;
         var patt1 = /\d+/g;
         var result = str.match(patt1);
+
         result = Number(result) + 1;
         labelArr[i].htmlFor = str.replace(str.match(patt1), result);
-        console.log(labelArr[i].htmlFor);
       }
     }
 
-    console.log(inputArr);
     for (i = 0; i < inputArr.length; i++) {
       if (inputArr[i].id) {
         var str = inputArr[i].id;
         var patt1 = /\d+/g;
         var result = str.match(patt1);
+
         result = Number(result) + 1;
         inputArr[i].id = str.replace(str.match(patt1), result);
-        console.log(inputArr[i].id);
-
         inputArr[i].value = '';
       }
       if (inputArr[i].name) {
         var str = inputArr[i].name;
         var patt1 = /\d+/g;
         var result = str.match(patt1);
+
         result = Number(result) + 1;
         inputArr[i].name = str.replace(str.match(patt1), result);
-        console.log(inputArr[i].name);
       }
     }
 
     $('.short-answer-template:last').removeClass('template');
   });
 
-  // $('.edit_survey').submit(function(e) {
-  //   e.preventDefault;
-  //
-  //   return false;
-  // });
+  $('.arrow-up').click(function(e) {
+    var arrowUp = $(this);
+    var questionDiv = arrowUp.parent()[0];
 
+    var index = $('.short-answer-template').index(questionDiv);
+    //
+    console.log(index);
+
+    var parent = $('.short-answer-template')[Number(index)];
+    //
+    var prev = $('.short-answer-template')[Number(index) - 1];
+
+    console.log(prev);
+
+    var next = $('.short-answer-template')[Number(index) + 1];
+
+    console.log(next);
+
+    $(parent).insertBefore($(prev));
+
+    //------------------------------
+
+    // var questionFieldInputs = $(".short-answer-template:last > .question_fields");
+    //
+    // var inputArr = $('input', questionFieldInputs);
+    //
+    // console.log(inputArr[1]);
+    // console.log(inputArr[1].value);
+
+    //
+    // if (parentPrev.className === 'short-answer-template') {
+    //   console.log('can move up');
+    // }
+
+    // parent.insertBefore()
+  });
 
 });
