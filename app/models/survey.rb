2978262fe73d93807eced1bf4c594fd2716f32dd
Owen <owenrham@gmail.com>
2015-02-15 10:01:54 -0500

    Fix required button

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -6,6 +6,7 @@ class Survey < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
 
+
   accepts_nested_attributes_for :questions,
     :allow_destroy => :true,
     :reject_if => proc { |attributes| attributes['text'].blank?}
